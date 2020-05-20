37b9e7741dd20a07975b1969101456f502cc1a1a
Harukaichii <h445chen@edu.uwaterloo.ca>
2020-05-20 19:29:50 -0400

    fixed css styling

--- src/components/book/BookInfo.js
+++ src/components/book/BookInfo.js
@@ -5,6 +5,7 @@ import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import CloseIcon from "@material-ui/icons/Close";
@@ -16,6 +17,7 @@ import ImportContactsIcon from "@material-ui/icons/ImportContacts";
 import { makeStyles } from "@material-ui/core/styles";
 import Pill from "../../util/Pill";
 import MyButton from "../../util/MyButton";
+import Proudtalelogo from "../../assets/images/proudtalelogo.png";
 
 export default function BookInfo() {
   const classes = useStyles();
@@ -128,7 +130,11 @@ export default function BookInfo() {
             cook together with your guests. Add 1 cup of frozen peas along with
             the mussels, if you like.
           </Typography>
-          <Typography onClick={handleClickOpen} component="a">
+          <Typography
+            onClick={handleClickOpen}
+            component="a"
+            classes={{ root: classes.hoverShadow }}
+          >
             Copyright Details
           </Typography>
           <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
@@ -139,26 +145,42 @@ export default function BookInfo() {
             >
               <CloseIcon />
             </MyButton>
-            <DialogContent className={classes.dialogContent}>
-              THIS IS STATIC CONTENT FOR NOW. WILL BE REPLACED WITH PROPS LATER.
-              Copyright © 2010 by Bill Shakespeare All rights reserved. No part
-              of this publication may be reproduced, distributed, or transmitted
-              in any form or by any means, including photocopying, recording, or
-              other electronic or mechanical methods, without the prior written
-              permission of the publisher, except in the case of brief
-              quotations embodied in critical reviews and certain other
-              noncommercial uses permitted by copyright law. For permission
-              requests, write to the publisher, addressed “Attention:
-              Permissions Coordinator,” at the address below. Imaginary Press
-              1233 Pennsylvania Avenue San Francisco, CA 94909
-              www.imaginarypress.com Ordering Information: Quantity sales.
-              Special discounts are available on quantity purchases by
-              corporations, associations, and others. For details, contact the
-              publisher at the address above. Orders by U.S. trade bookstores
-              and wholesalers. Please contact Big Distribution: Tel: (800)
-              800-8000; Fax: (800) 800-8001 or visit www.bigbooks.com. Printed
-              in the United States of America
-            </DialogContent>
+
+            <Grid container justify="center">
+              <Grid item>
+                <Avatar
+                  alt="proudtale logo"
+                  src={Proudtalelogo}
+                  className={classes.logo}
+                  classes={{
+                    root: classes.logoRoot,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <DialogContent className={classes.dialogContent}>
+                  THIS IS STATIC CONTENT FOR NOW. WILL BE REPLACED WITH PROPS
+                  LATER. Copyright © 2010 by Bill Shakespeare All rights
+                  reserved. No part of this publication may be reproduced,
+                  distributed, or transmitted in any form or by any means,
+                  including photocopying, recording, or other electronic or
+                  mechanical methods, without the prior written permission of
+                  the publisher, except in the case of brief quotations embodied
+                  in critical reviews and certain other noncommercial uses
+                  permitted by copyright law. For permission requests, write to
+                  the publisher, addressed “Attention: Permissions Coordinator,”
+                  at the address below. Imaginary Press 1233 Pennsylvania Avenue
+                  San Francisco, CA 94909 www.imaginarypress.com Ordering
+                  Information: Quantity sales. Special discounts are available
+                  on quantity purchases by corporations, associations, and
+                  others. For details, contact the publisher at the address
+                  above. Orders by U.S. trade bookstores and wholesalers. Please
+                  contact Big Distribution: Tel: (800) 800-8000; Fax: (800)
+                  800-8001 or visit www.bigbooks.com. Printed in the United
+                  States of America
+                </DialogContent>
+              </Grid>
+            </Grid>
           </Dialog>
         </Grid>
       </Grid>
@@ -206,6 +228,19 @@ const useStyles = makeStyles((theme) => ({
   },
   dialogContent: {
     padding: 30,
+    lineHeight: "30px",
+  },
+  logo: {
+    margin: theme.spacing(3),
+  },
+  logoRoot: {
+    height: "80px",
+    width: "80px",
+  },
+  hoverShadow: {
+    "&:hover": {
+      textShadow: "2px -1px 3px rgba(0,0,0,0.24)",
+    },
   },
 }));
 
